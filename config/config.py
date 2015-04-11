3afffa78dc1fe89cdc5a5b71874a51493758f16f
Piotr Oleksy <piotr.olek@o2.pl>
2015-04-11 17:52:04 +0200

    initial

---
+++ config/config.py
@@ -0,0 +1,28 @@
+
+
+downloadURLs = {
+	'bwa': 'http://sourceforge.net/projects/bio-bwa/files/bwa-0.7.12.tar.bz2',
+	'pickard': 'https://github.com/broadinstitute/picard/tarball/master/picard.tar.gz',
+}
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def blue(st):
+    return bcolors.OKBLUE + st + bcolors.ENDC
+
+def warning(st):
+    return bcolors.WARNING + st + bcolors.ENDC
+
+def ok(st):
+    return bcolors.OKGREEN + st + bcolors.ENDC
+
+def fail(st):
+    return bcolors.FAIL + st + bcolors.ENDC
---
+++ config/setConfiguration.py
@@ -0,0 +1,11 @@
+from colorama import init
+
+from config import blue, warning, ok, fail
+
+def main():
+    init()
+    print blue("Welcome to NGS configurator\n")
+    pass
+
+if __name__ == '__main__':
+    main()
