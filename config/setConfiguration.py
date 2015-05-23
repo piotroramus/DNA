3f9263347bed2080c27239949952872fd1236756
Piotr Oleksy <piotr.olek@o2.pl>
2015-05-23 11:10:00 +0200

    contextmanaging intensifies

--- config/setConfiguration.py
+++ config/setConfiguration.py
@@ -32,10 +32,11 @@ def prepare_input_files(args=None):
     def clean_downloads():
         pass
 
-    def cd_2_up():  # TODO convert to context manager
-        print blue('cd ../..')
-        os.chdir('../..')
-        print '\tcwd: ' + os.getcwd()
+    # def cd_2_up():  # TODO convert to context manager
+    #
+    #     print blue('cd ../..')
+    #     os.chdir('../..')
+    #     print '\tcwd: ' + os.getcwd()
 
     mkdir('downloads')
     mkdir('hg19')
@@ -44,29 +45,30 @@ def prepare_input_files(args=None):
             return False
     if not extract_file(os.path.join('downloads', 'chromFa.tar.gz'), 'hg19/chromFa'):
         return False
-    print blue('cd hg19/chromFa')
-    os.chdir('hg19/chromFa')
-    print '\tcwd: ' + os.getcwd()
-    command = 'cat chr1.fa chr2.fa chr3.fa chr4.fa chr5.fa chr6.fa chr7.fa chr8.fa chr9.fa\
-        chr10.fa chr11.fa chr12.fa chr13.fa chr14.fa chr15.fa chr16.fa chr17.fa chr18.fa\
-        chr19.fa chr20.fa chr21.fa chr22.fa chrX.fa chrY.fa chrM.fa > hg19.fa'
-    print blue('\t' + command)
-    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = p.communicate()
-    if err:
-        print fail('ERROR: \n' + err + '\n')
-        cd_2_up()
-        return False
-    print ok('  ok\n')
-    command = 'bwa index -a bwtsw -p hg19 hg19.fa'
-    print blue('\t' + command)
-    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = p.communicate()
-    if err:
-        print fail('ERROR: \n' + err + '\n')
-        cd_2_up()
-        return False
-    cd_2_up()
+    # print blue('cd hg19/chromFa')
+    # os.chdir('hg19/chromFa')
+    # print '\tcwd: ' + os.getcwd()
+    with cwd(joiner('hg19', 'chromFa')):
+        command = 'cat chr1.fa chr2.fa chr3.fa chr4.fa chr5.fa chr6.fa chr7.fa chr8.fa chr9.fa\
+            chr10.fa chr11.fa chr12.fa chr13.fa chr14.fa chr15.fa chr16.fa chr17.fa chr18.fa\
+            chr19.fa chr20.fa chr21.fa chr22.fa chrX.fa chrY.fa chrM.fa > hg19.fa'
+        print blue('\t' + command)
+        p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if err:
+            print fail('ERROR: \n' + err + '\n')
+            # cd_2_up()
+            return False
+        print ok('  ok\n')
+        command = 'bwa index -a bwtsw -p hg19 hg19.fa'
+        print blue('\t' + command)
+        p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if err:
+            print fail('ERROR: \n' + err + '\n')
+            # cd_2_up()
+            return False
+        # cd_2_up()
     print ok('  ok\n')
     return True
 
