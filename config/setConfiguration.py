f58d4c917995d9fcf4fd3a29bc358864978beb88
Piotr Oleksy <piotr.olek@o2.pl>
2015-05-23 09:22:27 +0200

    preparation of alaignment reference input files.

--- config/setConfiguration.py
+++ config/setConfiguration.py
@@ -2,6 +2,7 @@ import argparse
 from colorama import init
 import os
 import urllib2
+import subprocess
 from config import blue, warning, ok, fail, downloadURLs
 
 
@@ -21,22 +22,84 @@ def main():
     configure.get(args.purpose, no_such_arg)(args=args)
 
 
-def prepare_input_files(args=None):
-    def mkdir(path):
-        if not os.path.exists(path):
-            os.makedirs(path)
+def mkdir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
 
+
+def prepare_input_files(args=None):
     def clean_downloads():
         pass
 
+    def cd_2_up():
+        print blue('cd ../..')
+        os.chdir('../..')
+        print '\tcwd: ' + os.getcwd
+
     mkdir('downloads/')
-    download_file('hg19', downloadURLs['hg19'], 'downloads/')  # TODO destination should be configurable
-    pass
+    mkdir('hg19/')
+    if not exists('downloads/chromFa.tar.gz'):
+        if not download_file('hg19', downloadURLs['hg19'], 'downloads/'):  # TODO destination should be configurable
+            return False
+    if not extract_file('downloads/chromFa.tar.gz', 'hg19/chromFa'):
+        return False
+    print blue('cd hg19/chromFa')
+    os.chdir('hg19/chromFa')
+    print '\tcwd: ' + os.getcwd()
+    command = 'cat chr1.fa chr2.fa chr3.fa chr4.fa chr5.fa chr6.fa chr7.fa chr8.fa chr9.fa\
+        chr10.fa chr11.fa chr12.fa chr13.fa chr14.fa chr15.fa chr16.fa chr17.fa chr18.fa\
+        chr19.fa chr20.fa chr21.fa chr22.fa chrX.fa chrY.fa chrM.fa > hg19.fa'
+    print blue('\t' + command)
+    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    if err:
+        print fail('ERROR: \n' + err + '\n')
+        cd_2_up()
+        return False
+    print ok('  ok\n')
+    command = 'bwa index -a bwtsw -p hg19 hg19.fa'
+    print blue('\t' + command)
+    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    if err:
+        print fail('ERROR: \n' + err + '\n')
+        cd_2_up()
+        return False
+    cd_2_up()
+    print ok('  ok\n')
+    return True
+
+
+def extract_file(path, destination):
+    print ' Extracting file: ' + path + ' into: ' + destination
+    mkdir(destination)
+    command = 'tar xf ' + path + ' -C ' + destination
+    print blue('\t' + command)
+    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    if err:
+        print fail('ERROR: \n' + err + '\n')
+        return False
+    print ok('  ok\n')
+    return True
+
+
+def exists(path):
+    # print ' Checking for existence of: ' + path
+    # if os.path.isfile(path):
+    #     print ok('Already here: \n    ' + path
+    #     return True
+    # print warning('Not here!')
+    # return False
+    return True
 
 
 def full_configuration(args=None):
-    print warning('Not implemented yet!\n')
-    pass
+    if not install_tools(args):
+        print fail('nah')
+    if not prepare_input_files(args):
+        print fail('nah')
+    print ok('ok')
 
 
 def install_tools(args=None):
