aeeefa994c633db37cffe0aeeef08b2b93dad43d
Piotr Oleksy <piotr.olek@o2.pl>
2015-11-09 18:27:25 +0100

    moved tools to dict for easier lookup

--- config/tools.py
+++ config/tools.py
@@ -7,3 +7,12 @@ ngs_tools = [ Tool('bwa', 'bwa'),
           Tool('Java', 'apps/java'),
           Tool('Picard', 'apps/picard/1.140'),
           Tool('GATK', 'tools/gatk') ]
+
+
+def dictify(tools):
+	dictionary = {}
+	for tool in tools:
+		dictionary[tool.name] = tool.module_path
+
+
+ngs_tools_dict = dictify(ngs_tools)
