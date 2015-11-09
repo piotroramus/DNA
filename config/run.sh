2d2011e237158f7ba5c1494373b31d16410ff3d8
Piotr Oleksy <piotr.olek@o2.pl>
2015-11-09 17:58:18 +0100

    removed links not to be used anymore

--- config/run.sh
+++ config/run.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-python $HOME/dev/dna/config/setConfiguration.py -download $STORAGE/download -hg $SCRATCH/hg -apps $STORAGE/apps install
+python $HOME/dev/dna/config/setConfiguration.py -download $STORAGE/download -hg $SCRATCH/hg
