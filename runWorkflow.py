9deb2285904c0ba59c45a6a13fe23efb0342c04f
Piotr Oleksy <piotr.olek@o2.pl>
2015-11-10 13:50:06 +0100

    removed module add java from Picard runs, as it is not needed

--- runWorkflow.py
+++ runWorkflow.py
@@ -19,7 +19,8 @@ def actual_alignment(args):
 def SAM_to_BAM_conversion(args):
     blue('Going for STAGE_2 - SAM_to_BAM_conversion')
     with cwd(joiner(args.hg, 'chromFa')):
-        cmd = 'module add ' + ngs_tools_dict['Java'] + ' && module add ' + ngs_tools_dict['Picard'] + ' && $PICARDRUN -Xmx4g -Djava.io.tmpdir=/tmp SortSam SO=coordinate INPUT=out.sam OUTPUT=output.bam VALIDATION_STRINGENCY=LENIENT CREATE_INDEX=true'
+        cmd = 'module add ' + ngs_tools_dict['Picard'] + ' && $PICARDRUN SortSam SO=coordinate INPUT=out.sam OUTPUT=output.bam VALIDATION_STRINGENCY=LENIENT CREATE_INDEX=true'
+        # cmd = 'module add ' + ngs_tools_dict['Picard'] + ' && $PICARDRUN -Xmx4g -Djava.io.tmpdir=/tmp SortSam SO=coordinate INPUT=out.sam OUTPUT=output.bam VALIDATION_STRINGENCY=LENIENT CREATE_INDEX=true'
         blue(cmd)
         run_command(cmd, Exception)
     ok('done')
@@ -28,7 +29,8 @@ def SAM_to_BAM_conversion(args):
 def marking_PCR_duplicates(args):
     blue('Going for STAGE_3 - marking_PCR_duplicates')
     with cwd(joiner(args.hg, 'chromFa')):
-        cmd = 'module add ' + ngs_tools_dict['Java'] + ' && module add ' + ngs_tools_dict['Picard'] + ' && $PICARDRUN -Xmx4g -Djava.io.tmpdir=/tmp MarkDuplicates INPUT=output.bam OUTPUT=output.marked.bam METRICS_FILE=metrics CREATE_INDEX=true VALIDATION_STRINGENCY=LENIENT'
+        cmd = 'module add ' + ngs_tools_dict['Picard'] + ' && $PICARDRUN MarkDuplicates INPUT=output.bam OUTPUT=output.marked.bam METRICS_FILE=metrics CREATE_INDEX=true VALIDATION_STRINGENCY=LENIENT'
+        # cmd = 'module add ' + ngs_tools_dict['Picard'] + ' && $PICARDRUN -Xmx4g -Djava.io.tmpdir=/tmp MarkDuplicates INPUT=output.bam OUTPUT=output.marked.bam METRICS_FILE=metrics CREATE_INDEX=true VALIDATION_STRINGENCY=LENIENT'
         blue(cmd)
         run_command(cmd, Exception)
     ok('done')
@@ -46,7 +48,8 @@ def local_realignment(args):
         blue(cmd)
         run_command(cmd, Exception)
         blue('\tpart3')
-        cmd = 'module add ' + ngs_tools_dict['Java'] + ' && module add ' + ngs_tools_dict['Picard'] + ' && $PICARDRUN -Djava.io.tmpdir=/tmp/flx-auswerter FixmateInformation INPUT=input.marked.realigned.bam OUTPUT=input_bam.marked.realigned.fixed.bam SO=coordinate VALIDATION_STRINGENCY=LENIENT CREATE_INDEX=true'
+        cmd = 'module add ' + ngs_tools_dict['Picard'] + ' && $PICARDRUN FixmateInformation INPUT=input.marked.realigned.bam OUTPUT=input_bam.marked.realigned.fixed.bam SO=coordinate VALIDATION_STRINGENCY=LENIENT CREATE_INDEX=true'
+        # cmd = 'module add ' + ngs_tools_dict['Picard'] + ' && $PICARDRUN -Djava.io.tmpdir=/tmp/flx-auswerter FixmateInformation INPUT=input.marked.realigned.bam OUTPUT=input_bam.marked.realigned.fixed.bam SO=coordinate VALIDATION_STRINGENCY=LENIENT CREATE_INDEX=true'
     ok('Done!')
 
 
