f076b26a93bcdae2d600e9e3f16c06647d3e517a
Piotr Oleksy <piotr.olek@o2.pl>
2015-11-15 12:53:55 +0100

    moved chrM.fa chromosome in reference fastq file, so that it matches dbnsp ref file from broad

--- config/setConfiguration.py
+++ config/setConfiguration.py
@@ -34,9 +34,9 @@ def prepare_input_files(args=None):
         raise Exception('Something went wrong while extracting the reference file.')
 
     with cwd(joiner(args.hg, 'chromFa')):
-        command = 'cat chr1.fa chr2.fa chr3.fa chr4.fa chr5.fa chr6.fa chr7.fa chr8.fa chr9.fa\
+        command = 'cat chrM.fa chr1.fa chr2.fa chr3.fa chr4.fa chr5.fa chr6.fa chr7.fa chr8.fa chr9.fa\
             chr10.fa chr11.fa chr12.fa chr13.fa chr14.fa chr15.fa chr16.fa chr17.fa chr18.fa\
-            chr19.fa chr20.fa chr21.fa chr22.fa chrX.fa chrY.fa chrM.fa > hg19.fa'
+            chr19.fa chr20.fa chr21.fa chr22.fa chrX.fa chrY.fa > hg19.fa'
         run_command(command, Exception)
         command = 'module add ' + ngs_tools_dict['bwa'] + ' && bwa index -a bwtsw -p hg19 hg19.fa'
         run_command(command, Exception)
