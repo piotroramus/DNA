1197345ba17f572600c46d2e88d3bd59fc85c79e
Mikolaj Nowak <mikolaj.nowak93@gmail.com>
2015-12-11 18:22:43 +0100

    Added setup

--- webapp_scripts/webapp.sh
+++ webapp_scripts/webapp.sh
@@ -1,8 +1,9 @@
-if [ $# -eq 1 ]; then
+if [ $# -eq 2 ]; then
   DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
   OUTPUT_FILE=$1
+  SEQ_URL=$2
 
-  python $HOME/dev/dna/runWorkflow.py -download $STORAGE/download -hg $SCRATCH/hg -1 > $OUTPUT_FILE
+  python $HOME/DNA/runWorkflow.py -download $STORAGE/download -hg $SCRATCH/hg --seq-read-url $SEQ_URL -1 > $OUTPUT_FILE
 else
-	echo "Incorrect number of arguments passed $# expected 1"
+	echo "Incorrect number of arguments passed $# expected 2"
 fi
